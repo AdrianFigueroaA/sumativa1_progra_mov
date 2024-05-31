a408954ee9e79910ba37004f71fa9e01789c2084
adrian <revanskyw@gmail.com>
2024-05-31 13:05:06 -0400

    se agrega la funcionalidad en la vista estrenos de ller info desde la api de peliculas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-tab2',
+//   templateUrl: 'tab2.page.html',
+//   styleUrls: ['tab2.page.scss'],
+// })
+// export class Tab2Page {
+//   constructor() {}
+// }
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
-
+export class Tab2Page implements OnInit {
   constructor() {}
 
+  ngOnInit() {
+    this.mostrarPosters(document.getElementById('container-card-estrenos'));
+  }
+
+  async mostrarPosters(elementoHTML: HTMLElement | null) {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNTA1MzE4MTZkMWU1ZGY0OGMzZWNlMzMwNTQ1MzM0NSIsInN1YiI6IjY2MjkxOWRmMTc2YTk0MDE3ZjgzNjM3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VLTYur3Ut34FSJWM_JmQigT2z72C_le5acTHO2d717Q',
+      },
+    };
+
+    try {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+        options
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error al consumir la API: ${response.status}`);
+      }
+
+      const datos = await response.json();
+
+      let html = '';
+      for (const pelicula of datos.results) {
+        const titulo = pelicula.title;
+        const imagen = pelicula.poster_path
+          ? `https://image.tmdb.org/t/p/w300${pelicula.poster_path}`
+          : 'imagen-no-disponible.jpg';
+
+        html += `
+          <ion-card   class="ion-text-center">
+            <img alt="Silhouette of mountains" src="${imagen}" />
+            <ion-card-header>
+              <ion-card-title>${titulo}</ion-card-title>
+            </ion-card-header>
+          </ion-card>
+        `;
+      }
+
+      if (elementoHTML) {
+        elementoHTML.innerHTML = html;
+      } else {
+        console.error('Elemento HTML no encontrado');
+      }
+    } catch (error) {
+      console.error('Error al obtener los datos:', error);
+    }
+  }
 }
